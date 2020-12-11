e82f77ec23abccf236aaf46fb2c86947857bcf09
vyloy <vyloy@qq.com>
2020-12-11 15:57:59 +0800

    changed top interval from 60 to 10 on linux

--- const_linux.go
+++ const_linux.go
@@ -1,17 +1,13 @@
 package shell
 
-import (
-	"strconv"
-)
-
 var (
 	NetState = Command{"netstat", "-pan"}
 	PS       = Command{"ps", "-eLf"}
 	M3PS     = Command{"ps", "-eLf"}
 	Disk     = Command{"df", "-hk"}
 	Top      = Command{WaitCommand, "top", "-bc",
-		"-d", strconv.Itoa(TOP_INTERVAL),
-		"-n", strconv.Itoa(SCRIPT_SPAN/TOP_INTERVAL + 1)}
+		"-d", "10",
+		"-n", "3"}
 	TopH = Command{WaitCommand, "top", "-bH",
 		"-n", "1",
 		"-p", DynamicArg}
