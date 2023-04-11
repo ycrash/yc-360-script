ed3681194a63b90d163436ada4e509c3f7ce04fd
Sandeep Sudheer <sandeep@tier1app.com>
2023-04-11 17:25:54 +0000

    + Modify Dmesg commands for Linux

--- const_linux.go
+++ const_linux.go
@@ -18,8 +18,8 @@ var (
 	TopH2               = Command{"/bin/sh", "-c", `"RESULT=$(top -bH -n 1) && echo \"$RESULT\" | head -n 4 && echo \"$RESULT\" | grep"`}
 	Top4M3              = Command{WaitCommand, "top", "-bc", "-n", "1"}
 	VMState             = Command{WaitCommand, "vmstat", DynamicArg, DynamicArg, `| awk '{cmd="(date +'%H:%M:%S')"; cmd | getline now; print now $0; fflush(); close(cmd)}'`}
-	DMesg               = Command{"/bin/sh", "-c", "dmesg -T --level=emerg,alert,crit,err,warn && dmesg -T --level=emerg,alert,crit,err,warn | tail -20"}
-	DMesg2              = Command{"/bin/sh", "-c", "dmesg -n 4 | tail -20"}
+	DMesg               = Command{"/bin/sh", "-c", "dmesg -T --level=emerg,alert,crit,err,warn | tail -20"}
+	DMesg2              = Command{"/bin/sh", "-c", "dmesg --level=emerg,alert,crit,err,warn | tail -20 | awk '{gsub(/\[[^]]*\]/,""); print strftime("[\%a \%b \%d \%H:\%M:\%S \%Y]", systime()-$(NF-1)), $0}'"}
 	GC                  = Command{"/bin/sh", "-c"}
 	AppendJavaCoreFiles = Command{"/bin/sh", "-c", "cat javacore.* > threaddump.out"}
 	AppendTopHFiles     = Command{"/bin/sh", "-c", "cat topdashH.* >> threaddump.out"}
