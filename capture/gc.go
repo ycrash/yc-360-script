aab1f1292d47cef36fdcf981f412f1cf2c110589
Sandeep Sudheer <sandeep@tier1app.com>
2023-04-20 07:10:29 +0000

    change %p logic for gcPath

--- capture/gc.go
+++ capture/gc.go
@@ -128,8 +128,8 @@ func processGCLogFile(gcPath string, out string, dockerID string, pid int) (gc *
 	}
 	// -Xloggc:/home/ec2-user/buggyapp/gc.%p.log
 	// /home/ec2-user/buggyapp/gc.pid2843.log
-	if strings.Contains(gcPath, `%p`) {
-		gcPath = strings.Replace(gcPath, `%p`, ""+strconv.Itoa(pid), 1)
+	if strings.Contains(gcPath, `%p`) && strings.Contains(gcPath, "pid") {
+		gcPath = strings.Replace(gcPath, `%p`, "pid"+strconv.Itoa(pid), 1)
 	}
 	if len(dockerID) > 0 {
 		err = shell.DockerCopy(out, dockerID+":"+gcPath)
